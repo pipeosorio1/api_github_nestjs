67bcc6415549769ff383fe60fd66a3130c96938e
Andres Felipe Osorio <pipeosorio13@gmail.com>
2024-05-31 08:48:41 -0500

    GITHUB_ACCESS_TOKEN no required

--- src/common/config/load.config.ts
+++ src/common/config/load.config.ts
@@ -5,7 +5,6 @@ const validationSchema = Joi.object({
   APP_PORT: Joi.number().default(3000),
   APP_NAME: Joi.string().required(),
   URL_GITHUB: Joi.string().required(),
-  GITHUB_ACCESS_TOKEN: Joi.string().empty(),
 });
 
 export const LoadConfig = ConfigModule.forRoot({
